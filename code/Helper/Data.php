16527d77398d95e8bbf053ab62155d9f323962d4
Pete McWilliams <pmcwilliams@augustash.com>
2012-09-02 23:45:14 -0500

    Refines module and allows enable/disable on frontend & admin

---
+++ code/Helper/Data.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Add Jquery/Jquery UI support
+ *
+ * @category    Ash
+ * @package     Ash_Core
+ * @copyright   Copyright (c) 2012 August Ash, Inc. (http://www.augustash.com)
+ * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+/**
+ * Core data helper
+ *
+ * @category    Ash
+ * @package     Ash_Jquery
+ * @author      August Ash Team <core@augustash.com>
+ */
+class Ash_Jquery_Helper_Data extends Mage_Core_Helper_Abstract
+{
+}
